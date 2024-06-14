7fe4927dc5321f8e2697b0ccb5e5dbe459381be3
aditya-jnu <121703339+aditya-jnu@users.noreply.github.com>
2024-06-14 13:16:34 +0530

    change

--- config/databse.js
+++ config/databse.js
@@ -2,7 +2,12 @@ const mongoose=require('mongoose')
 require('dotenv').config();
 
 const dbConnect=()=>{
-    mongoose.connect(process.env.MONGODB_URL,{
+    const uri = process.env.DATABASE_URL; // Check if DATABASE_URL is properly set
+        if (!uri) {
+            throw new Error('DATABASE_URL is not defined in environment variables');
+        }
+
+    mongoose.connect(uri,{
         useNewUrlParser:true,
         useUnifiedTopology:true
     }).then(()=>{
