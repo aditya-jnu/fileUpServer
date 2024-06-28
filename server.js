9009333d8b3f510a0327762c843612bb4a697343
aditya-jnu <121703339+aditya-jnu@users.noreply.github.com>
2024-06-29 00:16:22 +0530

    cors issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 
 const cors=require('cors');
-app.use(cors());
+app.use(cors({ origin: 'https://campuschitchat.netlify.app' }));
 
 require('dotenv').config();
 const PORT=process.env.PORT||3000;
